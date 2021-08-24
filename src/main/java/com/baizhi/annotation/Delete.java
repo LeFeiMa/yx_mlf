83f4d57d0a0e31a74f75daf7e6a7da17582e9606
mlf <17831132376@163.com>
2021-08-24 15:07:18 +0800

    这是第7次上传v7.0

--- src/main/java/com/baizhi/annotation/Delete.java
+++ src/main/java/com/baizhi/annotation/Delete.java
@@ -4,6 +4,7 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+//自定义注解哈哈哈哈  我是2020版本的idea。。。。。.
 //自定义注解哈哈哈哈  我是2021版本的idea
 //固定写法
 @Target(ElementType.METHOD)
